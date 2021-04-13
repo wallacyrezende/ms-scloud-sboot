8bd016ebfc35943c1d2082709d78ecdfbab08613
Wallacy Rezende <wallacyrezende@gmail.com>
2021-04-13 20:39:27 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/java/com/devwall/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.devwall.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
