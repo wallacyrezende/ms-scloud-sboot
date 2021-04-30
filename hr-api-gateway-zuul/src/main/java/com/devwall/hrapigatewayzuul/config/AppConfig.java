01d34459091f465098c7447316cfa1a2f8cea213
Wallacy Rezende <wallacyrezende@gmail.com>
2021-04-29 23:15:49 -0300

    Security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/devwall/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devwall/hrapigatewayzuul/config/AppConfig.java
@@ -1,12 +1,18 @@
 package com.devwall.hrapigatewayzuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope //atualiza var em tempo de exc com o actuator
 @Configuration
 public class AppConfig {
+	
+	@Value("${jwt.secret}")
+	private String jwtSecret;
 	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
--- hr-oauth/src/main/java/com/devwall/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/devwall/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,8 @@
 package com.devwall.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,9 +14,16 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Value("${oauth.client.name}")
+	private String clientName;
+	
+	@Value("${oauth.client.secret}")
+	private String clientSecret;
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -36,8 +45,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients
-		.inMemory().withClient("myappname123")
-		.secret(passwordEncoder.encode("myappsecret123"))
+		.inMemory().withClient(clientName)
+		.secret(passwordEncoder.encode(clientSecret))
 		.scopes("read", "write")
 		.authorizedGrantTypes("password")
 		.accessTokenValiditySeconds(86400);
